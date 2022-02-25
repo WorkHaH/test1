20a1b3bb4d297d13722a71e6d100ba73f0d50bc9
workhah <1436902829@qq.com>
2022-02-25 14:35:19 +0800

    创建test2

---
+++ src/com/workhah/test2.java
@@ -0,0 +1,11 @@
+package com.workhah;
+
+/**
+ * @author WorkHaH
+ * @date 2022/2/25
+ **/
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
