571d3f9957399337e9d6b8b53f3ee5d69f56e612
workhah <1436902829@qq.com>
2022-02-25 14:32:07 +0800

    修改了test.java

--- src/com/workhah/test.java
+++ src/com/workhah/test.java
@@ -1,8 +1,14 @@
 package com.workhah;
 
+import java.sql.SQLOutput;
+
 /**
  * @author WorkHaH
  * @date 2022/2/25
  **/
 public class test {
+    public static void main(String[] args) {
+        System.out.println(123);
+        System.out.println(234);
+    }
 }
